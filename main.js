aa1f50934b43ea73ed3847454731324200bb5bad
Elías Pereyra <107650997+eliaspereyrap@users.noreply.github.com>
2022-11-14 16:53:32 -0300

    Update main.js
    Añadir algunas reglas gramáticas para la generación de nombres y convertir algunas funciones a métodos estáticos de una clase

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 import { propuestas } from "./propuestas.js";
-
 // Botones para crear candidatos y votantes
 const cantCandidatos = document.getElementById("cantCandidatos");
 const cantVotantes = document.getElementById("cantVotantes");
 
 // Formularios para cada boton
-const formCandidatos = document.getElementById("formCandidatos");
-const formVotantes = document.getElementById("formVotantes");
-const formMostrarCand = document.getElementById("formMostrarCandidato");
-const formMostrarVot = document.getElementById("formMostrarVotante");
+const generarCandForm = document.getElementById("formCandidatos");
+const generarVotForm = document.getElementById("formVotantes");
+const mostrarCandForm = document.getElementById("formMostrarCandidato");
+const mostrarVotForm = document.getElementById("formMostrarVotante");
 
 // Textos para anunciar la creación de candidatos y votantes
 const anuncioCandGenerados = document.getElementById("anuncioCandGenerados");
@@ -28,54 +27,6 @@ const botones = document.querySelectorAll(".boton");
 
 // Variables que se van a inicializar cuando se rellenen los inputs necesarios
 let _cantCandidatos, _cantVotantes, _numCandidato, _numVotante;
-
-formCandidatos.addEventListener("submit", (event) => {
-    event.preventDefault();
-    _cantCandidatos = parseInt(cantCandidatos.value);
-    generarCandidatos(_cantCandidatos);
-});
-
-formVotantes.addEventListener("submit", (event) => {
-    event.preventDefault();
-    _cantVotantes = parseInt(cantVotantes.value);
-    generarVotantes(_cantVotantes);
-});
-
-btnVotar.addEventListener("click", votar);
-btnReset.addEventListener("click", reset);
-
-formMostrarCand.addEventListener("submit", (event) => {
-    event.preventDefault();
-    _numCandidato = numCandidato.value;
-    mostrarCandidato(_numCandidato);
-});
-
-formMostrarVot.addEventListener("submit", (event) => {
-    event.preventDefault();
-    _numVotante = numVotante.value;
-    mostrarVotante(_numVotante);
-});
-
-class Votante {
-    constructor(nombre, apellido, dni, ciudad, id, yaVoto) {
-        this.Nombre = nombre;
-        this.Apellido = apellido;
-        this.DNI = dni;
-        this.Ciudad = ciudad;
-        this.ID = id;
-        this.yaVoto = yaVoto;
-    }
-}
-
-class Candidato {
-    constructor(nombre, apellido, ideologia, propuestas, votosAcumulados) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.ideologia = ideologia;
-        this.propuestas = propuestas;
-        this.votosAcumulados = votosAcumulados;
-    }
-}
 
 //  Para asignar a cada candiato según se vayan creando
 const ideologias = [
@@ -104,31 +55,182 @@ let votando = false;
 // Variables para manejar el código asíncrono en la función "votar"
 let contando, esperarGanador, mostrarEmpate, return1, return2;
 
+generarCandForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    _cantCandidatos = parseInt(cantCandidatos.value);
+    Candidato.generar(_cantCandidatos);
+});
+
+generarVotForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    _cantVotantes = parseInt(cantVotantes.value);
+    Votante.generar(_cantVotantes);
+});
+
+mostrarCandForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    _numCandidato = numCandidato.value;
+    Candidato.presentarse(_numCandidato);
+});
+
+mostrarVotForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    _numVotante = numVotante.value;
+    Votante.presentarse(_numVotante);
+});
+
+btnVotar.addEventListener("click", votar);
+btnReset.addEventListener("click", reset);
+
+class Votante {
+    constructor(nombre, apellido, dni, ciudad, id, yaVoto) {
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.DNI = dni;
+        this.Ciudad = ciudad;
+        this.ID = id;
+        this.yaVoto = yaVoto;
+    }
+
+    static generar(_cantVotantes) {
+        if (votantesCreados)
+            return (msg.innerHTML = "Ya se crearon los votantes");
+
+        if (_cantVotantes === undefined || isNaN(parseInt(_cantVotantes)))
+            return (msg.innerHTML =
+                "Tenés que decirme cuántos votantes querés");
+
+        if (_cantVotantes <= 0)
+            return (msg.innerHTML = "Se necesita al menos un votante");
+
+        if (_cantVotantes > 10000)
+            return (msg.innerHTML = "Máximo 10.000 votantes");
+
+        msg.innerHTML = "";
+
+        // Asignar votantes
+        let id = 1;
+        for (let i = 0; i < _cantVotantes; i++) {
+            let votante = new Votante(
+                inventarSustProp(),
+                inventarSustProp(),
+                inventarDNI(),
+                inventarSustProp(),
+                id,
+                false
+            );
+            votantes.push(votante);
+            id++;
+        }
+
+        votantesCreados = true;
+        anuncioVotGenerados.innerHTML = `¡${_cantVotantes} votantes generados con éxito!`;
+    }
+
+    static presentarse(_numCandidato) {
+        if (!votantesCreados) return (msg.innerHTML = "No hay votantes");
+
+        if (isNaN(parseInt(_numCandidato)))
+            return (msg.innerHTML = "Dame un número de votante");
+
+        if (_numCandidato <= 0 || _numCandidato > votantes.length)
+            return (msg.innerHTML = "No existe ese número de votante");
+
+        const votante = votantes[_numCandidato - 1];
+        msg.innerHTML = `Soy ${votante.Nombre} ${votante.Apellido}, mi dni es ${votante.DNI} y soy de la ciudad de ${votante.Ciudad}.`;
+    }
+}
+
+class Candidato {
+    constructor(nombre, apellido, ideologia, propuestas, votosAcumulados) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ideologia = ideologia;
+        this.propuestas = propuestas;
+        this.votosAcumulados = votosAcumulados;
+    }
+
+    static generar(_cantCandidatos) {
+        if (candidatosCreados)
+            return (msg.innerHTML = "Ya se crearon los candidatos");
+
+        if (_cantCandidatos === undefined || isNaN(parseInt(_cantCandidatos)))
+            return (msg.innerHTML =
+                "Tenés que decirme cuántos candidatos querés");
+
+        if (_cantCandidatos < 2)
+            return (msg.innerHTML = "Se necesitan al menos 2 candidatos");
+
+        if (_cantCandidatos > cantIdeologias)
+            return (msg.innerHTML =
+                "No hay tantas ideologías por las que votar");
+
+        msg.innerHTML = "";
+        // Generar candidatos con ideologías aleatóreas
+        let indiceRandom;
+        let numObtenidos = [];
+        let ideologiaRandom;
+        let candidatoGenerado;
+
+        // Buscamos una ideología para cada candidato sin que se repitan
+        for (let i = 0; i < _cantCandidatos; i++) {
+            do {
+                indiceRandom = Math.floor(Math.random() * cantIdeologias);
+            } while (numObtenidos.indexOf(indiceRandom) !== -1);
+            /* Repetimos esto mientras ya se haya obtenido ese número en un ciclo anterior, así
+           nos aseguramos de no asignar la misma ideología a dos candidatos distintos. */
+            // Justamente para eso añadimos el número al array.
+            numObtenidos.push(indiceRandom);
+
+            ideologiaRandom = ideologias[indiceRandom];
+            candidatoGenerado = new Candidato(
+                inventarSustProp(),
+                inventarSustProp(),
+                ideologiaRandom,
+                generarPropuesta(),
+                0
+            );
+            candidatos.push(candidatoGenerado);
+        }
+
+        candidatosCreados = true;
+        anuncioCandGenerados.innerHTML = `¡${_cantCandidatos} candidatos generados con éxito!`;
+    }
+
+    static presentarse(_numCandidato) {
+        if (!candidatosCreados) return (msg.innerHTML = "No hay candidatos");
+        if (isNaN(parseInt(_numCandidato)))
+            return (msg.innerHTML = "Dame un número de candidato");
+        if (_numCandidato <= 0 || _numCandidato > candidatos.length)
+            return (msg.innerHTML = "No existe ese número de candidato");
+
+        const candidato = candidatos[_numCandidato - 1];
+        msg.innerHTML = `Soy ${candidato.nombre} ${candidato.apellido}, el candidato representante del partido ${candidato.ideologia}. <br><br> Nosotros ${candidato.propuestas}`;
+        window.scrollBy(0, 1000);
+    }
+}
+
 // Creamos dnis de 8 dígitos (desde 10 millones a 99.999.999)
 // Establecemos la cantidad máxima posible de distintos dni
 const MAX_DNI_POSIBLES = 89999999;
 let llamadasAInventarDNI = 0;
 let dniCreados = [];
 // Variable que va acontener los dni creados para que no se repitan
-
 function inventarDNI() {
-    if (llamadasAInventarDNI === MAX_DNI_POSIBLES) {
-        return "No se pueden crear más dni";
-    }
+    if (llamadasAInventarDNI === MAX_DNI_POSIBLES) return;
 
     const DIGITOS_DNI = 8;
     const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     const cantNums = nums.length;
-    // Array para añadir números que luego se convierte en string
     let dni = [];
-    // Número a añadir
     let num;
-    // CREAR DNI ÚNICOS E IRREPETIBLES:
-    do {
-        // Reinicializar dni como array vacio por si la condición del while se cumplió, ya que durante el proceso deja de ser un array
-        dni = [];
 
-        // Obtener el primer número y establecer que este no sea 0
+    // CREAR DNIs ÚNICOS E IRREPETIBLES:
+    do {
+        dni = [];
+        // Esto espor si la condición del while se cumplió, ya que durante el proceso deja de ser un array
+
+        // Primer número !== 0
         do {
             num = Math.floor(Math.random() * cantNums);
         } while (num === 0);
@@ -141,47 +243,130 @@ function inventarDNI() {
         }
 
         // Añadir puntos
-        // Después del segundo número
         dni.splice(2, 0, ".");
-        // Después del quinto número, teniendo en cuenta que ya añadimos el primer punto
         dni.splice(6, 0, ".");
 
-        // Convertir a string
-        dni = dni.toString();
-        // Dividir cadena por las comas y luego unirlas sin caracter que los separe
-        dni = dni.split(",").join("");
-
-        // Repetir la creacion deL dni mientras ese dni ya haya sido creado:
+        // Convertir a string sin comas o espacios que separn los números
+        dni = dni.toString().split(",").join("");
     } while (dniCreados.indexOf(dni) !== -1);
+    // Si este dni ya se obtuvo descartarlo y repetir el proceso
+
     dniCreados.push(dni);
     llamadasAInventarDNI++;
     return dni;
 }
 
 function inventarSustProp() {
-    const MIN_NUMBER = 3;
-    const MAX_NUMBER = 7;
-    // Cantidad de caracteres que va a tener el sustantivo propio
-    const numLetras = Math.round(
-        Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER
+    const MIN_NUM = 3;
+    const MAX_NUM = 7;
+    const nameLength = Math.round(
+        Math.random() * (MAX_NUM - MIN_NUM) + MIN_NUM
     );
     const vocales = ["a", "e", "i", "o", "u"];
-    const consonantes = ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","y","z",];
-    const cantidadVocales = vocales.length;
-    const cantidadConsonantes = consonantes.length;
+    const consonantes = [
+        "b",
+        "c",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "ñ",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ];
+    const cantVoc = vocales.length;
+    const cantCons = consonantes.length;
     let sustProp = "";
 
-    for (let i = 0; i < numLetras; i++) {
-        const letra =
-            i % 2 === 0
-                ? consonantes[Math.floor(Math.random() * cantidadConsonantes)]
-                : vocales[Math.floor(Math.random() * cantidadVocales)];
+    /* Número para que cada vez que se llama a la función se defina
+      si el sustantivo debe empezar con una consonante o con una vocal */
+    let vocOrCons = Math.round(Math.random());
+    let letra;
+
+    for (let i = 0; i < nameLength; i++) {
+        letra =
+            // Si vocOrCons vale 0 empieza por una consonante, sino por una vocal
+            i % 2 === vocOrCons
+                ? consonantes[Math.floor(Math.random() * cantCons)]
+                : vocales[Math.floor(Math.random() * cantVoc)];
+
+        // REGLAS GRMÁTICAS PARA LA Q Y LA G
+        switch (letra) {
+            case "q":
+                // Si hay espacio para dos letras más después de la "q"
+                if (i + 2 < nameLength) {
+                    let eOrI = ["e", "i"];
+                    letra = "qu" + eOrI[Math.round(Math.random())];
+                    // Para impedir que quede armado "quii" o "quee" poner una consonante despúes:
+                    vocOrCons = Number(!vocOrCons);
+                    // Para saltar a la posicion correspondiente luego de añadir letras de más en la misma iteración
+                    // Añadimos 3 letras pero hacemos 2 incrementos porque el que falta lo hace el for automáticamente
+                    i += 2;
+                } else if (i + 2 === nameLength) {
+                    // Si es la anteúltima buscamos otra consonante
+                    do {
+                        letra =
+                            consonantes[Math.floor(Math.random() * cantCons)];
+                    } while (letra === "q");
+                }
+                break;
+
+            case "e":
+            case "i":
+                if (sustProp[i - 1] === "g" && i + 1 < nameLength) {
+                    let añadirU = Boolean(Math.round(Math.random()));
+                    if (añadirU) {
+                        let dieresis = Boolean(Math.round(Math.random()));
+                        letra = dieresis ? "ü" + letra : "u" + letra;
+                        vocOrCons = Number(!vocOrCons);
+                        i++;
+                    }
+                }
+                break;
+        }
         sustProp += letra;
     }
 
-    const primeraLetra = sustProp.slice(0, 1).toUpperCase();
-    const resto = sustProp.substring(1, numLetras);
-    sustProp = primeraLetra + resto;
+    // Evitar terminaciones raras
+    if (
+        sustProp[sustProp.length - 1] === "ñ" ||
+        sustProp[sustProp.length - 1] === "w" ||
+        sustProp[sustProp.length - 1] === "h" ||
+        sustProp[sustProp.length - 1] === "q"
+    ) {
+        let ultCons;
+        do {
+            ultCons = consonantes[Math.floor(Math.random()) * cantCons];
+        } while (
+            ultCons === "ñ" ||
+            ultCons === "w" ||
+            ultCons === "h" ||
+            ultCons === "q"
+        );
+
+        sustProp = Array.from(sustProp);
+        sustProp.pop();
+        sustProp.push(ultCons);
+        sustProp = sustProp.toString().split(",").join("");
+    }
+
+    // Primer letra mayúscula
+    const primerLetra = sustProp.slice(0, 1).toUpperCase();
+    const resto = sustProp.substring(1, nameLength);
+    sustProp = primerLetra + resto;
     return sustProp;
 }
 
@@ -267,112 +452,6 @@ function generarPropuesta() {
     return propuestaFinal;
 }
 
-function generarVotantes(_cantVotantes) {
-    if (votantesCreados) return (msg.innerHTML = "Ya se crearon los votantes");
-
-    if (_cantVotantes === undefined || isNaN(parseInt(_cantVotantes)))
-        return (msg.innerHTML = "Tenés que decirme cuántos votantes querés");
-
-    if (_cantVotantes <= 0)
-        return (msg.innerHTML = "Se necesita al menos un votante");
-
-    if (_cantVotantes > MAX_DNI_POSIBLES)
-        return (msg.innerHTML = "Máximo 89.999.999 de votantes");
-
-    msg.innerHTML = "";
-
-    // Asignar votantes
-    let id = 1;
-    for (let i = 0; i < _cantVotantes; i++) {
-        let votante = new Votante(
-            inventarSustProp(),
-            inventarSustProp(),
-            inventarDNI(),
-            inventarSustProp(),
-            id,
-            false
-        );
-        votantes.push(votante);
-        id++;
-    }
-
-    votantesCreados = true;
-    anuncioVotGenerados.innerHTML = `¡${_cantVotantes} votantes generados con éxito!`;
-}
-
-function generarCandidatos(_cantCandidatos) {
-    if (candidatosCreados)
-        return (msg.innerHTML = "Ya se crearon los candidatos");
-
-    if (_cantCandidatos === undefined || isNaN(parseInt(_cantCandidatos)))
-        return (msg.innerHTML = "Tenés que decirme cuántos candidatos querés");
-
-    if (_cantCandidatos < 2)
-        return (msg.innerHTML = "Se necesitan al menos 2 candidatos");
-
-    if (_cantCandidatos > cantIdeologias)
-        return (msg.innerHTML = "No hay tantas ideologías por las que votar");
-
-    msg.innerHTML = "";
-
-    // Generar candidatos con ideologías aleatóreas
-    let indiceRandom;
-    let numObtenidos = [];
-    let ideologiaRandom;
-    let candidatoGenerado;
-
-    // Buscamos una ideología para cada candidato sin que se repitan
-    for (let i = 0; i < _cantCandidatos; i++) {
-        do {
-            indiceRandom = Math.floor(Math.random() * cantIdeologias);
-        } while (numObtenidos.indexOf(indiceRandom) !== -1);
-        /* Repetimos esto mientras ya se haya obtenido ese número en un ciclo anterior, así
-           nos aseguramos de no asignar la misma ideología a dos candidatos distintos. */
-        // Justamente para eso añadimos el número al array.
-        numObtenidos.push(indiceRandom);
-
-        ideologiaRandom = ideologias[indiceRandom];
-        candidatoGenerado = new Candidato(
-            inventarSustProp(),
-            inventarSustProp(),
-            ideologiaRandom,
-            generarPropuesta(),
-            0
-        );
-        candidatos.push(candidatoGenerado);
-    }
-
-    candidatosCreados = true;
-    anuncioCandGenerados.innerHTML = `¡${_cantCandidatos} candidatos generados con éxito!`;
-}
-
-function mostrarVotante(_id) {
-    if (!votantesCreados) return (msg.innerHTML = "No hay votantes");
-
-    if (isNaN(parseInt(_id)))
-        return (msg.innerHTML = "Dame un número de votante");
-
-    if (_id <= 0 || _id > votantes.length)
-        return (msg.innerHTML = "No existe ese número de votante");
-
-    const votante = votantes[_id - 1];
-    msg.innerHTML = `Soy ${votante.Nombre} ${votante.Apellido}, mi dni es ${votante.DNI} y soy de la ciudad de ${votante.Ciudad}.`;
-}
-
-function mostrarCandidato(_num) {
-    if (!candidatosCreados) return (msg.innerHTML = "No hay candidatos");
-
-    if (isNaN(parseInt(_num)))
-        return (msg.innerHTML = "Dame un número de candidato");
-
-    if (_num <= 0 || _num > candidatos.length)
-        return (msg.innerHTML = "No existe ese número de candidato");
-
-    let candidato = candidatos[_num - 1];
-    msg.innerHTML = `Soy ${candidato.nombre} ${candidato.apellido}, el candidato representante del partido ${candidato.ideologia}. <br><br> Nosotros ${candidato.propuestas}`;
-    window.scrollBy(0, 1000);
-}
-
 function votar() {
     if (votarUsada)
         return (msg.innerHTML = "Ya se votó. Inicia una nueva votación.");
@@ -442,6 +521,7 @@ function votar() {
         });
 
         let tiempoAnuncioEmpate = 3000;
+
         mostrarEmpate = setTimeout(
             () => (msg.innerHTML = anuncioEmpate),
             tiempoAnuncioEmpate
@@ -463,6 +543,7 @@ function votar() {
 
     // Si hubo empate que tarde más en aparecer el anuncio de espera del ganador
     let time = empate ? 6000 : 3000;
+
     esperarGanador = setTimeout(
         () => (msg.innerHTML += "<br>" + "Y el ganador es..."),
         time
@@ -478,6 +559,7 @@ function votar() {
 
     // Así mismo incrementar el tiempo del anuncio final en función de si se extendió el tiempo anterior o no
     let time2 = empate ? 9000 : 6000;
+
     if (empate) {
         return1 = setTimeout(() => {
             votando = false;
