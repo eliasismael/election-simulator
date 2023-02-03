13742a00a91af9a3e299c3760888b0bdd07f2fa0
Elías Pereyra <107650997+eliasismael@users.noreply.github.com>
2023-02-03 19:48:33 -0300

    Optimizacion

--- propuestas.js
+++ propuestas.js
@@ -1,146 +1,395 @@
-export const propuestas = {
-    propuestasEconomicas: {
-        intenciones: [
-            "vamos a ",
-            "queremos ",
-            "planeamos ",
-            "pensamos ",
-            "buscamos ",
-        ],
-        acciones: [
-            "aumentar ",
-            "reducir ",
-            "eliminar ",
-            "crear nuevos ",
-            "reforzar ",
-        ],
-        objetos: [
-            "impuestos ",
-            "bonos ",
-            "subsidios ",
-            "planes sociales ",
-            "aportes ",
-        ],
-        destinatarios: [
-            "para los sectores marginados ",
-            "para los grandes empresarios ",
-            "para los trabajadores ",
-            "para los autónomos ",
-            "para los emprendedores ",
-            "para los sindicalistas ",
-            "para los monotributistas ",
-        ],
-        conectores: [
-            "para ",
-            "con la intención de ",
-            "para así ",
-            "en post de ",
-            "con el objetivo de ",
-            "para poder ",
-            "en vista de ",
-        ],
-        propositos: [
-            "fomentar la inversión.",
-            "evitar el desempleo.",
-            "luchar contra la pobreza.",
-            "bajar la inflación.",
-            "impulsar el desarrollo.",
-            "fortalecer la industria nacional.",
-        ],
-    },
+// export const propuestas = {
+//   propuestasEconomicas: {
+//     intenciones: [
+//       "vamos a ",
+//       "queremos ",
+//       "planeamos ",
+//       "pensamos ",
+//       "buscamos ",
+//     ],
+//     acciones: [
+//       "aumentar ",
+//       "reducir ",
+//       "eliminar ",
+//       "crear nuevos ",
+//       "reforzar ",
+//     ],
+//     objetos: [
+//       "impuestos ",
+//       "bonos ",
+//       "subsidios ",
+//       "planes sociales ",
+//       "aportes ",
+//     ],
+//     destinatarios: [
+//       "para los sectores marginados ",
+//       "para los grandes empresarios ",
+//       "para los trabajadores ",
+//       "para los autónomos ",
+//       "para los emprendedores ",
+//       "para los sindicalistas ",
+//       "para los monotributistas ",
+//     ],
+//     conectores: [
+//       "para ",
+//       "con la intención de ",
+//       "para así ",
+//       "en post de ",
+//       "con el objetivo de ",
+//       "para poder ",
+//       "en vista de ",
+//     ],
+//     propositos: [
+//       "fomentar la inversión.",
+//       "evitar el desempleo.",
+//       "luchar contra la pobreza.",
+//       "bajar la inflación.",
+//       "impulsar el desarrollo.",
+//       "fortalecer la industria nacional.",
+//     ],
+//   },
 
-    propuestasPoliticas: {
-        intenciones: [
-            "Vamos a ",
-            "Queremos ",
-            "Planeamos ",
-            "Pensamos ",
-            "Buscamos ",
-        ],
-        acciones: [
-            "disolver ",
-            "pactar ",
-            "impulsar ",
-            "construir ",
-            "reformar ",
-        ],
-        objetos: [
-            "la cámara ",
-            "el ministerio ",
-            "la secretaría ",
-            "la subsecretaría ",
-        ],
-        destinatarios: [
-            "de diputados ",
-            "de senadores ",
-            "de ministros ",
-            "de jueces ",
-            "de legisladores ",
-        ],
-        conectores: [
-            "para ",
-            "con la intención de ",
-            "para así ",
-            "en post de ",
-            "con el objetivo de ",
-            "para poder ",
-            "en vista de ",
-        ],
-        propositos: [
-            "tener más democracia.",
-            "estar mejor representados.",
-            "tener mayor participacion popular.",
-            "luchar contra la corrupción.",
-            "descentralizar el poder.",
-        ],
-    },
+//   propuestasPoliticas: {
+//     intenciones: [
+//       "Vamos a ",
+//       "Queremos ",
+//       "Planeamos ",
+//       "Pensamos ",
+//       "Buscamos ",
+//     ],
+//     acciones: ["disolver ", "pactar ", "impulsar ", "construir ", "reformar "],
+//     objetos: [
+//       "la cámara ",
+//       "el ministerio ",
+//       "la secretaría ",
+//       "la subsecretaría ",
+//     ],
+//     destinatarios: [
+//       "de diputados ",
+//       "de senadores ",
+//       "de ministros ",
+//       "de jueces ",
+//       "de legisladores ",
+//     ],
+//     conectores: [
+//       "para ",
+//       "con la intención de ",
+//       "para así ",
+//       "en post de ",
+//       "con el objetivo de ",
+//       "para poder ",
+//       "en vista de ",
+//     ],
+//     propositos: [
+//       "tener más democracia.",
+//       "estar mejor representados.",
+//       "tener mayor participacion popular.",
+//       "luchar contra la corrupción.",
+//       "descentralizar el poder.",
+//     ],
+//   },
 
-    propuestasSociales: {
-        intenciones: [
-            "Y también vamos a ",
-            "También queremos ",
-            "Y planeamos ",
-            "Para finalizar pensamos ",
-            "Y en lo social buscamos ",
-        ],
-        acciones: [
-            "construir ",
-            "inaugurar ",
-            "mejorar ",
-            "diseñar ",
-            "reformar ",
-        ],
-        objetos: [
-            "hospitales ",
-            "universidades ",
-            "museos ",
-            "centros culturales ",
-            "espacios verdes ",
-            "espacios recreacionales ",
-        ],
-        destinatarios: [
-            "en los barrios humildes ",
-            "en las grandes ciudades ",
-            "en la zonas urbanas ",
-            "en las zonas rurales ",
-            "en los pueblos aislados ",
-        ],
-        conectores: [
-            "para ",
-            "con la intención de ",
-            "para así ",
-            "en post de ",
-            "con el objetivo de ",
-            "para poder ",
-            "en vista de ",
-        ],
-        propositos: [
-            "fomentar el turismo.",
-            "expandir la cultura.",
-            "acercar posibilidades.",
-            "brindar oportunidades.",
-            "garantizar servicios.",
-            "tener una sociedad más inclusiva.",
-        ],
-    },
+//   propuestasSociales: {
+//     intenciones: [
+//       "Y también vamos a ",
+//       "También queremos ",
+//       "Y planeamos ",
+//       "Para finalizar pensamos ",
+//       "Y en lo social buscamos ",
+//     ],
+//     acciones: ["construir ", "inaugurar ", "mejorar ", "diseñar ", "reformar "],
+//     objetos: [
+//       "hospitales ",
+//       "universidades ",
+//       "museos ",
+//       "centros culturales ",
+//       "espacios verdes ",
+//       "espacios recreacionales ",
+//     ],
+//     destinatarios: [
+//       "en los barrios humildes ",
+//       "en las grandes ciudades ",
+//       "en la zonas urbanas ",
+//       "en las zonas rurales ",
+//       "en los pueblos aislados ",
+//     ],
+//     conectores: [
+//       "para ",
+//       "con la intención de ",
+//       "para así ",
+//       "en post de ",
+//       "con el objetivo de ",
+//       "para poder ",
+//       "en vista de ",
+//     ],
+//     propositos: [
+//       "fomentar el turismo.",
+//       "expandir la cultura.",
+//       "acercar posibilidades.",
+//       "brindar oportunidades.",
+//       "garantizar servicios.",
+//       "tener una sociedad más inclusiva.",
+//     ],
+//   },
+// };
+
+const propuestasEconomicas = {
+  intenciones: [
+    "Vamos a ",
+    "Queremos ",
+    "Planeamos ",
+    "Pensamos ",
+    "Buscamos ",
+  ],
+  acciones: [
+    "aumentar ",
+    "reducir ",
+    "eliminar ",
+    "crear nuevos ",
+    "reforzar ",
+  ],
+  objetos: [
+    "impuestos ",
+    "bonos ",
+    "subsidios ",
+    "planes sociales ",
+    "aportes ",
+  ],
+  destinatarios: [
+    "para los sectores marginados ",
+    "para los grandes empresarios ",
+    "para los trabajadores ",
+    "para los autónomos ",
+    "para los emprendedores ",
+    "para los sindicalistas ",
+    "para los monotributistas ",
+  ],
+  conectores: [
+    "para ",
+    "con la intención de ",
+    "para así ",
+    "en post de ",
+    "con el objetivo de ",
+    "para poder ",
+    "en vista de ",
+  ],
+  propositos: [
+    "fomentar la inversión.",
+    "evitar el desempleo.",
+    "luchar contra la pobreza.",
+    "bajar la inflación.",
+    "impulsar el desarrollo.",
+    "fortalecer la industria nacional.",
+  ],
 };
+
+const propuestasPoliticas = {
+  intenciones: [
+    "Vamos a ",
+    "Queremos ",
+    "Planeamos ",
+    "Pensamos ",
+    "Buscamos ",
+  ],
+  acciones: ["disolver ", "pactar ", "impulsar ", "construir ", "reformar "],
+  objetos: [
+    "la cámara ",
+    "el ministerio ",
+    "la secretaría ",
+    "la subsecretaría ",
+  ],
+  destinatarios: [
+    "de diputados ",
+    "de senadores ",
+    "de ministros ",
+    "de jueces ",
+    "de legisladores ",
+  ],
+  conectores: [
+    "para ",
+    "con la intención de ",
+    "para así ",
+    "en post de ",
+    "con el objetivo de ",
+    "para poder ",
+    "en vista de ",
+  ],
+  propositos: [
+    "tener más democracia.",
+    "estar mejor representados.",
+    "tener mayor participacion popular.",
+    "luchar contra la corrupción.",
+    "descentralizar el poder.",
+  ],
+};
+
+const propuestasSociales = {
+  intenciones: [
+    "Y también vamos a ",
+    "También queremos ",
+    "Y planeamos ",
+    "Para finalizar pensamos ",
+    "Y en lo social buscamos ",
+  ],
+  acciones: ["construir ", "inaugurar ", "mejorar ", "diseñar ", "reformar "],
+  objetos: [
+    "hospitales ",
+    "universidades ",
+    "museos ",
+    "centros culturales ",
+    "espacios verdes ",
+    "espacios recreacionales ",
+  ],
+  destinatarios: [
+    "en los barrios humildes ",
+    "en las grandes ciudades ",
+    "en la zonas urbanas ",
+    "en las zonas rurales ",
+    "en los pueblos aislados ",
+  ],
+  conectores: [
+    "para ",
+    "con la intención de ",
+    "para así ",
+    "en post de ",
+    "con el objetivo de ",
+    "para poder ",
+    "en vista de ",
+  ],
+  propositos: [
+    "fomentar el turismo.",
+    "expandir la cultura.",
+    "acercar posibilidades.",
+    "brindar oportunidades.",
+    "garantizar servicios.",
+    "tener una sociedad más inclusiva.",
+  ],
+};
+
+const propuestas = [
+  propuestasEconomicas,
+  propuestasPoliticas,
+  propuestasSociales,
+];
+
+export default propuestas;
+
+function generarPropuesta() {
+  let frase = "";
+  for (let i = 0; i < propuestas.length; i++) {
+    const tipoDePropuesta = propuestas[i]; // objeto
+    const parteDelaPropuesta = Object.keys(tipoDePropuesta); // [ intenciones,acciones,etc ]
+
+    for (let j = 0; j < parteDelaPropuesta.length; j++) {
+      let posiblesParteDelTexto = tipoDePropuesta[parteDelaPropuesta[j]]; // ["queremos", "pensamos","planeamos",etc]
+      let randomIndex = Math.floor(
+        Math.random() * posiblesParteDelTexto.length
+      );
+      let texto = posiblesParteDelTexto[randomIndex];
+      frase += texto;
+    }
+    frase += "\n";
+  }
+
+  console.log(frase);
+}
+
+function generarPropuesta() {
+  let frase = "";
+  for (let i = 0; i < propuestas.length; i++) {
+    let tipoDePropuesta = propuestas[i];
+    let randomIndex;
+
+    randomIndex = Math.floor(
+      Math.random() * tipoDePropuesta.intenciones.length
+    );
+    frase += tipoDePropuesta.intenciones[randomIndex];
+
+    randomIndex = Math.floor(Math.random() * tipoDePropuesta.acciones.length);
+    frase += tipoDePropuesta.acciones[randomIndex];
+
+    randomIndex = Math.floor(Math.random() * tipoDePropuesta.objetos.length);
+    frase += tipoDePropuesta.objetos[randomIndex];
+
+    randomIndex = Math.floor(
+      Math.random() * tipoDePropuesta.destinatarios.length
+    );
+    frase += tipoDePropuesta.destinatarios[randomIndex];
+
+    randomIndex = Math.floor(Math.random() * tipoDePropuesta.conectores.length);
+    frase += tipoDePropuesta.conectores[randomIndex];
+
+    randomIndex = Math.floor(Math.random() * tipoDePropuesta.propositos.length);
+    frase += tipoDePropuesta.propositos[randomIndex];
+    frase += "\n";
+  }
+  console.log(frase);
+}
+
+generarPropuesta();
+
+/*  // Propuesta económica
+  let eco = Object.entries(propuestas.propuestasEconomicas);
+  // (Array de [ [prop, [value]],[prop, [value]], [prop, [value]]... ])
+
+  // El 1 es constante porque siempre accedemos a los valores de la respectiva propiedad,
+  // y estos siempre son el índice 1 de cada subarray
+  let ecoProps = [
+    eco[0][1],
+    eco[1][1],
+    eco[2][1],
+    eco[3][1],
+    eco[4][1],
+    eco[5][1],
+  ];
+
+  // Recorremos el array de cada propiedad pata pbtener aleatóreamente un elemento de cada una
+  let ecoValues = ecoProps.map((elements) => {
+    let numElement = Math.floor(Math.random() * elements.length);
+    return elements[numElement];
+  });
+
+  // Convertir el array a string, quitar las comas y juntar los caracteres sin elementos de por medio
+  let ecoFinal = ecoValues.toString().split(",").join("");
+
+  // Repetir el proceso por cada tipo de propuesta
+  // Propuesta política
+  let pol = Object.entries(propuestas.propuestasPoliticas);
+
+  let polProps = [
+    pol[0][1],
+    pol[1][1],
+    pol[2][1],
+    pol[3][1],
+    pol[4][1],
+    pol[5][1],
+  ];
+
+  let polValues = polProps.map((elements) => {
+    let numElement = Math.floor(Math.random() * elements.length);
+    return elements[numElement];
+  });
+
+  let polFinal = polValues.toString().split(",").join("");
+
+  // Propuesta social
+  let soc = Object.entries(propuestas.propuestasSociales);
+
+  let socProps = [
+    soc[0][1],
+    soc[1][1],
+    soc[2][1],
+    soc[3][1],
+    soc[4][1],
+    soc[5][1],
+  ];
+
+  let socValues = socProps.map((elements) => {
+    let numElement = Math.floor(Math.random() * elements.length);
+    return elements[numElement];
+  });
+
+  let socFinal = socValues.toString().split(",").join("");
+
+  let propuestaFinal = ecoFinal + "<br><br>" + polFinal + "<br><br>" + socFinal;
+  return propuestaFinal; */
